e8471a76caa9b2a923e897041680ed234121e52e
signaturekt <kevin97truong@gmail.com>
2024-10-05 21:50:04 -0700

    Add get_meal_by_name that queries meals by name and returns the recipe

--- Request.py
+++ Request.py
@@ -1,6 +1,7 @@
 import requests, json
 from Category import Category
 from Meal import Meal
+from Recipe import Recipe
 
 # API base URL
 BASE_URL = "https://www.themealdb.com/api/json/v1/1"
@@ -39,3 +40,30 @@ def get_meal_by_category(category : Category):
     else:
         print('An error has occured')
     return meals
+
+def get_meal_by_name(name):
+    """
+    Get meal by name from themealdb
+    """
+    url = f'{BASE_URL}/search.php?s={name}'
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        json = r.json()
+        m = json['meals']
+        i = 1
+        ingredients = []
+        measures = []
+        while m[0][f'strIngredient{i}'] != "":
+            ingredients.append(m[0][f'strIngredient{i}'])
+            measures.append(m[0][f'strMeasure{i}'])
+            i += 1
+        instructions = m[0]['strInstructions']
+        meal = m[0]['strMeal']
+        cateorgy = m[0]['strCategory']
+        id = m[0]['idMeal']
+        image_URL = m[0]['strMealThumb']
+        recipe = Recipe(id, meal, cateorgy, instructions, ingredients, measures, image_URL)
+    else:
+        print('an error has occured')
+    return recipe
